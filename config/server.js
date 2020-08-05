58a30ad482ea61c21dffbbcd5ba19efc267cf876
Vlad Cazacu <vlad@cazacu.me>
2020-08-05 09:55:35 +0200

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6f217bf08cf3272fe45404a884c66b5d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8f59b52f-bf83-43b6-bb2a-92497db9dc12'
+};
